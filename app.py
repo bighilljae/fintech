b8edb8ac29c7aca58fc2288bbaf96b4854ea0c0d
hanjae.jea <hanjae.jea@gmail.com>
2019-06-28 16:17:15 +0900

    upload

--- app.py
+++ app.py
@@ -10,11 +10,18 @@ def index():
 
 @app.route('/backtesting.html')
 def backtest():
-    print(request.args.get('id'))
     if request.args.get('id') is None:
         return render_template('backtesting.html')
     id = int(request.args.get('id')) - 1
     return render_template('backtesting.html', item1=id_list[id]['item1'], item2=id_list[id]['item2'])
+
+@app.route('/modelUpload')
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save('model/' + f.filename)
+        return 'suc'
+    return 'fail'
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
