ef0d52062caee2250a2c5d73a5d44c973b7bf8a4
hanjae.jea <hanjae.jea@gmail.com>
2019-06-28 14:55:35 +0900

    start python

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+id_list = [{'item1': 'https://datastudio.google.com/embed/reporting/1BPUO86dh2Kdu5jNIdVepIh-73ACGDkr7/page/jr4s',
+            'item2': 'https://datastudio.google.com/embed/reporting/18UA4eqZiruDn02J0mR2cLICn9bZImgQK/page/144s'}]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/backtesting.html')
+def backtest():
+    id = int(request.args.get('id')) - 1
+    return render_template('backtesting.html', id_list[id])
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
